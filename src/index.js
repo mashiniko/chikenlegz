8b88b3880af78bf34bc42a123e4743162b599810
henrykash <henrykariuki003@gmail.com>
2023-06-13 12:24:05 +0300

    server setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-console.log("hello clg")
-let name="mashniko"
-console.log (name)
-let number=69
-console.log (number)
+const http = require('http');
+
+const server = http.createServer(function(request, response) {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('We are finally the hello world devs gurus');
+});
+
+const port = 3000;
+server.listen({ port: port, host: 'localhost' }, function() {
+  console.log(`Server is running on port : ${port}`);
+}) 
